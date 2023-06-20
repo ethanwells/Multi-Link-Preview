25b7565e61bc2973e3a1b436e7e03f24f97c84c8
ethan <ethanwells@Ethans-MBP-3.attlocal.net>
2023-06-20 03:52:14 -0400

    commit

--- app.py
+++ app.py
@@ -67,9 +67,11 @@ def create_multi_link():
     
     # generate unique id for image
     uniqueId = bson.Binary(uuid.uuid4().bytes, bson.binary.UUID_SUBTYPE)
-    
+    print(f"uniqueID: {uniqueId}")
+
     blob = bucket.blob(f"{uniqueId}.jpg")  # create blob
     blob.upload_from_filename("multi-link-preview.jpg")  # upload image to blob
+    print(f"google cloud blob: https://storage.googleapis.com/multi-link-preview-images/{uniqueId}.jpg")
 
     # insert new imageID-to-links mapping entry into MongoDB
     doc = {"_id": uniqueId, "links": links}
